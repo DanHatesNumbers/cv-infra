53eae234f9659620b22fb6810ffa99af5a518d35
Daniel Murphy <danhatesnumbers@gmail.com>
2018-12-16 19:44:49 +0000

    Added hosting bucket, cloudfront distro, r53 cname, ACM cert and IAM user for CircleCI

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ terraform {
 }
 
 provider "aws" {
+    region = "us-east-1"
+	alias = "aws_us"
+}
+
+provider "aws" {
     region = "${var.aws_region}"
 }
 
@@ -75,4 +80,231 @@ resource "aws_s3_bucket" "state-bucket" {
       }
     }
   }
+}
+
+data "aws_iam_policy_document" "hosting-bucket-policy" {
+  statement {
+    sid = "ensure-private-read-write"
+
+    actions = [
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+    ]
+
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    resources = ["arn:aws:s3:::danhatesnumbers-cv-hosting/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+
+      values = [
+        "public-read",
+        "public-read-write",
+      ]
+    }
+  }
+
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::danhatesnumbers-cv-hosting/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::danhatesnumbers-cv-hosting"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"]
+    }
+  }
+}
+
+resource "aws_s3_bucket" "hosting-bucket" {
+	bucket = "danhatesnumbers-cv-hosting"
+	acl    = "private"
+  	policy = "${data.aws_iam_policy_document.hosting-bucket-policy.json}"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    abort_incomplete_multipart_upload_days = 14
+
+    expiration {
+      expired_object_delete_marker = true
+    }
+
+    noncurrent_version_transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    noncurrent_version_expiration {
+      days = 365
+    }
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {}
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.hosting-bucket.bucket_regional_domain_name}"
+    origin_id   = "${local.s3_origin_id}"
+
+    s3_origin_config {
+  		origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
+	}
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "CV.pdf"
+
+  aliases = ["cv.danhatesnumbers.co.uk"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${local.s3_origin_id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${local.s3_origin_id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+	  geo_restriction {
+	  	locations = []
+	  	restriction_type = "none"
+	  }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "${aws_acm_certificate.cert.arn}"
+	minimum_protocol_version = "TLSv1.2_2018"
+	ssl_support_method = "sni-only"
+  }
+}
+
+resource "aws_acm_certificate" "cert" {
+  provider = "aws.aws_us"
+  domain_name = "cv.danhatesnumbers.co.uk"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_route53_record" "cert_validation" {
+  name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
+  zone_id = "${var.hosted_zone_id}"
+  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
+  ttl     = 60
+}
+
+resource "aws_route53_record" "cv_domain" {
+  zone_id = "${var.hosted_zone_id}"
+  name = "cv.danhatesnumbers.co.uk"
+  type = "A"
+
+  alias {
+    name = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
+    zone_id = "${aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_iam_user" "circleci_deployment" {
+	name = "circledi_deployment"
+	path = "/cv/"
+}
+
+resource "aws_iam_access_key" "circleci" {
+  user = "${aws_iam_user.circleci_deployment.name}"
+}
+
+resource "aws_iam_user_policy" "circleci" {
+  name = "s3put"
+  user = "${aws_iam_user.circleci_deployment.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1544988926533",
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::danhatesnumbers-cv-hosting/CV.pdf"
+    }
+  ]
+}
+EOF
 }
